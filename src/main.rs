13d87b81406e40e60db042be937b64bee378a34e
トリナー <toliner.rkt@gmail.com>
2018-04-30 10:39:35 +0900

    init commandline parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#[macro_use]
+extern crate clap;
+
+use clap::{App, Arg, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let app = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!());
+
+    let matches = app.get_matches();
+
 }
